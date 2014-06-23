f190e664e1f77c77d899a9bd721fc35d0fa0489f
Jonathan Marshall <jmarshall@never.you.mind>
2014-06-24 10:48:20 +1200

    sampling, done.

--- sampling.R
+++ sampling.R
@@ -1,128 +1,35 @@
-## mean 80, sd = 8
-## CI 64 and 96 (+/- 2SD)
-
-# prob selecting sample outside these values, based on these and different sampling
-par(mfrow=c(2,2))
-# calc z values
-z1<-(64-80)/8
-z2<-(96-80)/8
-pnorm(z2)-pnorm(z1)
-
-
-yvals10<-rnorm(10,80,8)
- plot(density(yvals10))
- rug(yvals10)
-abline(v=64)
-abline(v=96)
-yvals100<-rnorm(100,80,8)
- plot(density(yvals100))
- rug(yvals100)
-abline(v=64)
-abline(v=96)
-yvals1000<-rnorm(1000,80,8)
- plot(density(yvals1000))
- rug(yvals1000)
-abline(v=64)
-abline(v=96)
-yvals10000<-rnorm(10000,80,8)
-  plot(density(yvals10000))
-  rug(yvals10000)
-abline(v=64)
-abline(v=96)
-
-par(mfrow=c(2,3))
-
-yvals10<-rnorm(10,80,8)
-plot(density(yvals10),main="",xlab="")
-rug(yvals10,side=1,col="red",lwd=1,ticksize=0.2)
-abline(v=64)
-abline(v=96)
-yvals20<-rnorm(20,80,8)
-plot(density(yvals20),main="",xlab="")
-rug(yvals20,side=1,col="red",lwd=1,ticksize=0.2)
-abline(v=64)
-abline(v=96)
-yvals30<-rnorm(30,80,8)
-plot(density(yvals30),main="",xlab="")
-rug(yvals30,side=1,col="red",lwd=1,ticksize=0.2)
-abline(v=64)
-abline(v=96)
-yvals40<-rnorm(40,80,8)
-plot(density(yvals40),main="",xlab="")
-rug(yvals40,side=1,col="red",lwd=1,ticksize=0.2)
-abline(v=64)
-abline(v=96)
-yvals50<-rnorm(50,80,8)
-plot(density(yvals50),main="",xlab="")
-rug(yvals50,side=1,col="red",lwd=1,ticksize=0.2)
-abline(v=64)
-abline(v=96)
-yvals100<-rnorm(100,80,8)
-plot(density(yvals100),main="",xlab="")
-rug(yvals100,side=1,col="red",lwd=1,ticksize=0.2)
-abline(v=64)
-abline(v=96)
-
-
-plot(yvals100, col= ifelse(yvals100 >= 96, "red", 
-                         ifelse(yvals100 <= 64,"blue", "black")),pch=16)
-abline(h=64,col="blue")
-abline(h=96,col="red")
-
-plot(yvals20, col= ifelse(yvals20 >= 96, "red", 
-                           ifelse(yvals20 <= 64,"blue", "black")),pch=16)
-abline(h=64,col="blue")
-abline(h=96,col="red")
-
-hist(yvals100,breaks=seq(50,120,2))
-abline(v=64,col="red")
-abline(v=96,col="red")
-
-hist(yvals20,breaks=seq(50,120,2))
-abline(v=64,col="red")
-abline(v=96,col="red")
-
-
-plot(yvals1000, col= ifelse(yvals1000 >= 96, "red", 
-                          ifelse(yvals1000 <= 64,"blue", "black")),pch=16)
-abline(h=64,col="blue")
-abline(h=96,col="red")
-
-hist(yvals1000,breaks=seq(50,120,2))
-abline(v=64,col="red")
-abline(v=96,col="red")
-
 ## adjust p values
 
 a <- 5 # mean - use the same one
 s <- 2 # sd
-n <- 1 # sample size
-xbar <- rnorm(10,mean=a,sd=s) # means randomly drawn from the above normal distribution
-pvals<-2*(1-pnorm(xbar,mean=a,sd=s/sqrt(20)))
-hist(pvals)
+num_tests <- 20
+n <- 20 # sample size
+xsamples <- matrix(rnorm(num_tests * n, mean=a, sd=s), ncol=num_tests)
+
+par(mfrow=c(2,1))
+# boxplot of data against population mean
+boxplot(xsamples, xlim=c(0,num_tests+1))
+abline(h=a, col="red")
+
+# calculate sample means and ci's
+xbar <- apply(xsamples, 2, mean)
+ci   <- apply(xsamples, 2, function(x) { t.test(x)$conf.int })
+
+# plot means, ci's and p-values
+plot(xbar, xlim=c(0,num_tests+1), ylim=range(xsamples), pch=19)
+abline(h=a, col="red")
+for (i in 1:num_tests)
+  lines(c(i,i), ci[,i])
+pvals <- apply(xsamples, 2, function(x) { t.test(x, mu=a)$p.value })
+text(1:num_tests, max(ci)+0.5, round(pvals, 2))
 
 padj<-p.adjust(pvals, method = "bonferroni", n = length(pvals))
+text(1:num_tests, min(ci)-2, round(padj, 2))
 
-# c("holm", "hochberg", "hommel", "bonferroni", "BH", "BY",
-#   "fdr", "none")
-
-#padj<-p.adjust(pvals, method = "BH", n = length(pvals))
-
+# plot p-values and adjusted p-values
 plot(pvals)
 abline(h=0.05)
 
 plot(padj)
-abline(h=0.05)
-
-padj
-
-pv<-do.call( rbind, replicate(10, t.test(rnorm(2,mean=10,sd=s),rnorm(100,mean=10,sd=s)), simplify=FALSE ) )
-pvals<-(as.vector(as.numeric(pv[,3])))
-
-plot(pvals,ylim=c(0,max(pvals)))
-abline(h=0.05)
-
-padj<-p.adjust(pvals, method = "bonferroni", n = length(pvals))
-plot(padj,ylim=c(0,max(padj)))
 abline(h=0.05)
 
